c8442ade86473057903010793c69dc6332a9c53c
acoppes <acoppes@eac1e5b8-72ba-4c6d-b6cc-dc585ae6e3ab>
2011-02-01 14:06:29 +0000

    changed JnlpAppletLoader to NewAppletLoader changed RealJnlpAppletLoader to JnlpAppletLoader extracted method which creates the AppletParameters from the JnlpInfo
    git-svn-id: https://svn.gemserk.com/jnlpappletloader/trunk/jnlpappletloader@39 eac1e5b8-72ba-4c6d-b6cc-dc585ae6e3ab

--- src/main/java/org/lwjgl/util/applet/JnlpAppletLoader.java
+++ src/main/java/org/lwjgl/util/applet/JnlpAppletLoader.java
@@ -3,307 +3,116 @@ package org.lwjgl.util.applet;
 import java.applet.Applet;
 import java.applet.AppletStub;
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
-import java.awt.GridLayout;
-import java.awt.Image;
-import java.awt.MediaTracker;
-import java.io.File;
-import java.net.MalformedURLException;
+import java.io.InputStream;
 import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.lwjgl.util.applet.tests.JnlpParserTest.JarInfo;
+import org.lwjgl.util.applet.tests.JnlpParserTest.JnlpInfo;
+import org.w3c.dom.Document;
 
 public class JnlpAppletLoader extends Applet implements AppletStub {
 
 	private static final long serialVersionUID = -2459790398016588477L;
 
-	FileInfoProvider fileInfoProvider;
+	private JnlpInfo jnlpInfo;
 
-	JarUtil jarUtil = new JarUtil();
+	private URL codeBase;
 
-	JarDownloader jarDownloader;
-
-	Progress progress = new Progress(100);
-
-	private Thread renderThread;
-
-	private Thread loaderThread;
-
-	private ProgressPanel progressPanel;
-
-	private Cache cache;
-
-	// @Override
-	// public void paint(Graphics g) {
-	// super.paint(g);
-	//
-	// g.setColor(Color.WHITE);
-	// g.fillRect(0, 0, getWidth(), getHeight());
-	//
-	// g.setColor(Color.BLACK);
-	//
-	// String status = "" + progress.getPercentage() + "%";
-	// if (!"".equals(progress.getStatus()))
-	// status += " - " + progress.getStatus();
-	//
-	// g.drawString(status, getWidth() / 2 - status.length() * 3, getHeight() / 2);
-	// }
+	Map<String, String> appletParameters = new HashMap<String, String>();
 
 	@Override
 	public void init() {
 
-		setIgnoreRepaint(true);
-
-		AppletParametersUtil appletParametersUtil = new AppletParametersUtil(this);
-
-		final AppletParameters appletParameters = new AppletParametersProxy(appletParametersUtil).getAppletParameters();
-
-		setLayout(new GridLayout(1, 1));
-
-		progressPanel = new ProgressPanel();
-
-		progressPanel.setIgnoreRepaint(true);
-
-		progressPanel.setSize(getWidth(), getHeight());
-		progressPanel.setProgress(progress);
-
-		progressPanel.setLogo(getImage(appletParameters.getLogo()));
-		progressPanel.setProgressBar(getImage(appletParameters.getProgessbar()));
-
-		add(progressPanel);
-
-		renderThread = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					while (true) {
-						progressPanel.repaint();
-						// repaint();
-						Thread.sleep(100);
-					}
-				} catch (InterruptedException e) {
-					// System.out.println("animation thread interrupted");
-				}
-			}
-		});
-		renderThread.start();
-
-		loaderThread = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				load();
-				cleanup();
-			}
-		});
-		loaderThread.start();
-	}
-
-	private void cleanup() {
-
-		remove(progressPanel);
-
-		renderThread.interrupt();
-
 		try {
-			renderThread.join(2000);
-		} catch (InterruptedException e) {
-			throw new RuntimeException(e);
-		}
+			String jnlpHref = getParameter("al_jnlp");
 
-	}
+			URL jnlpUrl = new URL(jnlpHref);
 
-	private void load() {
+			InputStream jnlpInputStream = jnlpUrl.openStream();
 
-		URL codeBase = internalGetCodeBase();
+			DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
 
-		fileInfoProvider = new FileInfoProviderRemoteImpl(codeBase);
+			Document document = documentBuilder.parse(jnlpInputStream);
 
-		JarUtil jarUtils = new JarUtil();
+			jnlpInfo = new JnlpParser(document).parse();
 
-		String tempFolder = System.getProperty("java.io.tmpdir") + File.separator + "lwjgltmp" + File.separator;
+			appletParameters.putAll(getAppletParametersFromJnlpInfo(jnlpInfo));
 
-		File cacheFile = new File(tempFolder + "cache");
+			codeBase = new URL(jnlpInfo.codeBase);
 
-		jarDownloader = new JarDownloader(codeBase, tempFolder);
-		jarDownloader.setJarUtils(jarUtils);
+			System.out.println(appletParameters);
+			
+			AppletLoader appletLoader = new AppletLoader();
+			appletLoader.setStub(this);
 
-		// parseParameters
-
-		// get jars info?
-
-		// check cache and remove already downloader jars from jars to download?
-
-		AppletParametersUtil appletParametersUtil = new AppletParametersUtil(this);
-
-		progress.update("getting parameters", 5);
-
-		final AppletParameters appletParameters = new AppletParametersProxy(appletParametersUtil).getAppletParameters();
-
-		// List<URL> urls = convertToUrls(getCodeBase(), appletParameters.getJars());
-		//		
-		// for (URL url : urls)
-		// System.out.println(url);
-
-		List<FileInfo> jarFiles = new ArrayList<FileInfo>();
-
-		progress.update("Getting jars information...", 5);
-
-		jarFiles.addAll(getFilesInfo(appletParameters.getJars(), 10, 5));
-
-		List<FileInfo> nativeFiles = new ArrayList<FileInfo>();
-
-		List<FileInfo> files = new ArrayList<FileInfo>();
-
-		files.addAll(jarFiles);
-		files.addAll(nativeFiles);
-
-		progress.update("Checking cached jars...", 15);
-
-		cache = Cache.load(cacheFile);
-		CacheFilter cacheFilter = new CacheFilter(cache);
-
-		List<FileInfo> newFiles = cacheFilter.removeCachedFiles(files);
-
-		// download jars
-
-		progress.update("Downloading jars...", 25);
-
-		downloadFiles(newFiles, 25, 60);
-
-		// progress.update("Extracting jars...", 55);
-
-		// extract jars
-
-		// validate jars?
-
-		// update cache?
-
-		if (newFiles.size() > 0) {
-
-			progress.update("Updating cache...", 85);
-
-			updateCache(newFiles, 10, 85);
-
-			Cache.save(cache, cacheFile);
-		}
-
-		// another stuff
-
-		// update class loader and classpath...
-
-		progress.update("Updating classpath...", 95);
-
-		URL[] urls = getLocalJarUrls(tempFolder, files);
-
-		// updating class path : taltal.jar
-		for (int i = 0; i < urls.length; i++) {
-			System.out.println("URL for class loader " + urls[i]);
-			progress.update("Updating classpath - " + files.get(i).getFileName(), 95);
-		}
-
-		ClassLoader classLoader = new URLClassLoader(urls, Thread.currentThread().getContextClassLoader());
-
-		// set system library, etc
-
-		// switch applet
-
-		switchApplet(classLoader, appletParameters);
-	}
-
-	private void updateCache(List<FileInfo> files, int progressTotal, int progressOffset) {
-		int count = files.size();
-		for (int i = 0; i < files.size(); i++) {
-			FileInfo fileInfo = files.get(i);
-			cache.add(fileInfo);
-			progress.update("Updating cache entry - " + fileInfo.getFileName(), progressOffset + progressTotal / count * i);
-		}
-	}
-
-	private void downloadFiles(List<FileInfo> files, int progressOffset, int progressTotal) {
-		int count = files.size();
-		for (int i = 0; i < files.size(); i++) {
-			FileInfo fileInfo = files.get(i);
-			// update progress based on file info content length?
-			// System.out.println("Downloading " + fileInfo.getFileName() + " [" + fileInfo.getContentLength() + "]" + "...");
-			progress.update("Downloading - " + fileInfo.getFileName(), progressOffset + progressTotal / count * i);
-			jarDownloader.download(fileInfo);
-		}
-
-	}
-	
-	private List<FileInfo> getFilesInfo(List<String> urls, int progressTotal, int progressOffset) {
-		List<FileInfo> files = new ArrayList<FileInfo>();
-
-		int count = urls.size();
-
-		for (int i = 0; i < urls.size(); i++) {
-			progress.update("Getting jar information - " + urls.get(i), progressOffset + progressTotal / count * i);
-			files.add(fileInfoProvider.getFileInfo(urls.get(i)));
-		}
-		return files;
-	}
-
-	private URL[] getLocalJarUrls(String tempFolder, List<FileInfo> files) {
-		try {
-			URL[] urls = new URL[files.size()];
-			for (int i = 0; i < files.size(); i++)
-				urls[i] = new URL("file:" + tempFolder + files.get(i).getFileName());
-			return urls;
-		} catch (MalformedURLException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	private URL internalGetCodeBase() {
-		String codeBase = getParameter("al_codebase");
-		if (codeBase == null)
-			return this.getCodeBase();
-		try {
-			return new URL(codeBase);
-		} catch (MalformedURLException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	private void switchApplet(final ClassLoader classLoader, final AppletParameters appletParameters) {
-		try {
-			EventQueue.invokeAndWait(new Runnable() {
-				public void run() {
-					switchApplet(classLoader, appletParameters.getMain());
-					repaint();
-				}
-			});
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	/**
-	 * replace the current applet with the lwjgl applet using AppletStub and initialise and start it
-	 * 
-	 * @param appletClassName
-	 *            the applet class name to be loaded.
-	 */
-	@SuppressWarnings("unchecked")
-	protected void switchApplet(ClassLoader classLoader, String appletClassName) {
-		try {
-			Class appletClass = classLoader.loadClass(appletClassName);
-			Applet applet = (Applet) appletClass.newInstance();
-
-			applet.setStub(this);
-			applet.setSize(getWidth(), getHeight());
+			appletLoader.init();
+			appletLoader.start();
 
 			setLayout(new BorderLayout());
-			add(applet);
-			validate();
 
-			applet.init();
+			this.add(appletLoader);
 
-			applet.start();
 		} catch (Exception e) {
-			throw new RuntimeException("switch applet failed", e);
+			throw new RuntimeException(e);
 		}
+
+	}
+
+	private Map<String, String> getAppletParametersFromJnlpInfo(JnlpInfo jnlpInfo) {
+		Map<String, String> appletParameters = new HashMap<String, String>();
+		appletParameters.putAll(jnlpInfo.appletDescInfo.parameters);
+		
+		appletParameters.put("al_main", jnlpInfo.appletDescInfo.mainClassName);
+		appletParameters.put("al_title", jnlpInfo.appletDescInfo.name);
+
+		String al_jars = getJars(jnlpInfo.resources, "", false);
+		System.out.println("jars: " + al_jars);
+		appletParameters.put("al_jars", al_jars);
+
+		String al_linux = getJars(jnlpInfo.resources, "Linux", true);
+		System.out.println("Linux natives: " + al_linux);
+		appletParameters.put("al_linux", al_linux);
+
+		String al_windows = getJars(jnlpInfo.resources, "Windows", true);
+		System.out.println("Windows natives: " + al_windows);
+		appletParameters.put("al_windows", al_windows);
+
+		String al_mac = getJars(jnlpInfo.resources, "Mac OS", true);
+		System.out.println("Mac OS natives: " + al_mac);
+		appletParameters.put("al_mac", al_mac);
+		return appletParameters;
+	}
+
+	private String getJars(List<JarInfo> resources, String os, boolean nativeLib) {
+
+		StringBuilder stringBuilder = new StringBuilder();
+
+		for (int i = 0; i < resources.size(); i++) {
+			JarInfo jarInfo = resources.get(i);
+
+			if (jarInfo.nativeLib != nativeLib)
+				continue;
+
+			// starts with?
+			if (!"".equals(os) && !os.equals(jarInfo.os))
+				continue;
+
+			stringBuilder.append(jarInfo.href);
+			stringBuilder.append(", ");
+		}
+
+		if (stringBuilder.length() > 0)
+			stringBuilder.replace(stringBuilder.length() - 2, stringBuilder.length(), "");
+
+		String al_jars = stringBuilder.toString();
+		return al_jars;
 	}
 
 	@Override
@@ -311,36 +120,18 @@ public class JnlpAppletLoader extends Applet implements AppletStub {
 		resize(width, height);
 	}
 
-	/**
-	 * Get Image from path provided
-	 * 
-	 * @param s
-	 *            location of the image
-	 * @return the Image file
-	 */
-	protected Image getImage(String s) {
-		try {
-			URL url = Thread.currentThread().getContextClassLoader().getResource(s);
-
-			// if image not found in jar, look outside it
-			if (url == null)
-				url = new URL(getCodeBase(), s);
-
-			Image image = super.getImage(url);
-
-			// wait for image to load
-			MediaTracker tracker = new MediaTracker(this);
-			tracker.addImage(image, 0);
-			tracker.waitForAll();
-
-			// if no errors return image
-			if (tracker.isErrorAny())
-				throw new RuntimeException("failed to get image " + s);
-
-			return image;
-		} catch (Exception e) {
-			throw new RuntimeException("failed to get image " + s, e);
-		}
-
+	@Override
+	public URL getCodeBase() {
+		return codeBase;
 	}
+
+	@Override
+	public String getParameter(String name) {
+
+		if (appletParameters.containsKey(name))
+			return appletParameters.get(name);
+
+		return super.getParameter(name);
+	}
+
 }
