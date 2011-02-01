3a4f8eacff9337ebe36d029f9ea6b513bda6c27c
acoppes <acoppes@eac1e5b8-72ba-4c6d-b6cc-dc585ae6e3ab>
2011-02-01 14:05:00 +0000

    added some code lines to test the server.
    git-svn-id: https://svn.gemserk.com/jnlpappletloader/trunk/jnlpappletloader@33 eac1e5b8-72ba-4c6d-b6cc-dc585ae6e3ab

--- src/main/java/org/lwjgl/util/applet/tests/HttpRequestEncodingTest.java
+++ src/main/java/org/lwjgl/util/applet/tests/HttpRequestEncodingTest.java
@@ -6,16 +6,18 @@ import java.net.URLConnection;
 public class HttpRequestEncodingTest {
 
 	public static void main(String[] args) throws Exception {
-		
+
 		URL url = new URL("http://acoppes-laptop.local/prototipos/discoverthename-test/animation-0.0.2-SNAPSHOT.jar");
-		
+		// URL url = new URL("http://www.gemserk.com/private/prototipos/zombierockers-webstart-latest/animation-0.0.3-SNAPSHOT.jar");
+
 		URLConnection connection = url.openConnection();
-		
+
 		connection.addRequestProperty("Accept-Encoding", "pack200-gzip, gzip");
+		// connection.addRequestProperty("Accept-Encoding", "gzip, *;q=0");
 		connection.connect();
 
 		System.out.println(connection.getContentEncoding());
-		
+
 	}
 
 }
