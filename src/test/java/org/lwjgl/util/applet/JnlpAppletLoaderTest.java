7a6d276fe3b01b40f322ff750d1fe16e80e62661
acoppes <acoppes@eac1e5b8-72ba-4c6d-b6cc-dc585ae6e3ab>
2011-02-01 14:06:59 +0000

    added tests to test getJars method of JnlpAppletLoader which returns a list of comma separated jars based on a list of JnlpJarInfo
    git-svn-id: https://svn.gemserk.com/jnlpappletloader/trunk/jnlpappletloader@41 eac1e5b8-72ba-4c6d-b6cc-dc585ae6e3ab

--- src/test/java/org/lwjgl/util/applet/JnlpAppletLoaderTest.java
+++ src/test/java/org/lwjgl/util/applet/JnlpAppletLoaderTest.java
@@ -1,16 +1,21 @@
 package org.lwjgl.util.applet;
 
+import static org.junit.Assert.assertThat;
+
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hamcrest.core.IsEqual;
 import org.jmock.Mockery;
 import org.jmock.integration.junit4.JMock;
 import org.jmock.lib.legacy.ClassImposteriser;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.lwjgl.util.applet.JnlpParser.JnlpJarInfo;
 
 @RunWith(JMock.class)
+@SuppressWarnings("serial")
 public class JnlpAppletLoaderTest {
 
 	Mockery mockery = new Mockery() {
@@ -20,10 +25,32 @@ public class JnlpAppletLoaderTest {
 	};
 
 	@Test
-	public void testGetCodeBasedUrlForJarList() throws MalformedURLException {
+	public void shouldGetOnlyNativesForOnePlatform() throws MalformedURLException {
 		JnlpAppletLoader jnlpAppletLoader = new JnlpAppletLoader();
-		List<JnlpParser.JnlpJarInfo> resources = new ArrayList<JnlpParser.JnlpJarInfo>();
-		jnlpAppletLoader.getJars(resources, "", false);
+		
+		List<JnlpJarInfo> resources = new ArrayList<JnlpJarInfo>() {{
+			add(new JnlpJarInfo("lwjgl.jar", "Windows", false));
+			add(new JnlpJarInfo("lwjgl-win.jar", "Windows", true));
+			add(new JnlpJarInfo("lwjgl-linux.jar", "Linux", true));
+			add(new JnlpJarInfo("lwjgl-mac.jar", "Mac OS", true));
+		}};
+		
+		assertThat(jnlpAppletLoader.getJars(resources, "Windows", true), IsEqual.equalTo("lwjgl-win.jar"));
+	}
+	
+	@Test
+	public void shouldGetOnlyAllNotNativeDependencies() throws MalformedURLException {
+		JnlpAppletLoader jnlpAppletLoader = new JnlpAppletLoader();
+		
+		List<JnlpJarInfo> resources = new ArrayList<JnlpJarInfo>() {{
+			add(new JnlpJarInfo("lwjgl.jar", "", false));
+			add(new JnlpJarInfo("jinput.jar", "", false));
+			add(new JnlpJarInfo("lwjgl-win.jar", "Windows", true));
+			add(new JnlpJarInfo("lwjgl-linux.jar", "Linux", true));
+			add(new JnlpJarInfo("lwjgl-mac.jar", "Mac OS", true));
+		}};
+		
+		assertThat(jnlpAppletLoader.getJars(resources, "", false), IsEqual.equalTo("lwjgl.jar, jinput.jar"));
 	}
 
 
