5bf1949406303e6044a331a1030bc2b8bbc1a8f5
acoppes <acoppes@eac1e5b8-72ba-4c6d-b6cc-dc585ae6e3ab>
2011-02-01 14:02:12 +0000

    changed methods to be public
    git-svn-id: https://svn.gemserk.com/jnlpappletloader/trunk/jnlpappletloader@20 eac1e5b8-72ba-4c6d-b6cc-dc585ae6e3ab

--- src/main/java/org/lwjgl/util/applet/UrlUtils.java
+++ src/main/java/org/lwjgl/util/applet/UrlUtils.java
@@ -1,13 +1,10 @@
-/**
- * 
- */
 package org.lwjgl.util.applet;
 
 import java.net.URL;
 
 public class UrlUtils {
 
-	protected static String getFileName(URL url) {
+	public static String getFileName(URL url) {
 		return url.getFile().substring(url.getFile().lastIndexOf('/') + 1);
 	}
 
